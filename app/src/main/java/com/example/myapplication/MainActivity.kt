8ffe1942cef2fd906a7740bff266c1f363cf7af5
TakaDevCode <101286474+TakaDevCode@users.noreply.github.com>
2026-01-22 16:21:09 -0300

    Actualizacion formulario y implementacion de animacion para formularios

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -20,12 +21,13 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -38,11 +40,15 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
-import android.R.drawable.ic_secure as loginImageVector
+import kotlinx.coroutines.launch
+import com.example.myapplication.R.drawable.login_icon as loginImageVector
 
 
 class MainActivity : ComponentActivity() {
@@ -51,8 +57,22 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Inicial(
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            title = { Text("Ventana 1") },
+                            navigationIcon = {
+                                Icon(
+                                    painter = painterResource(id = loginImageVector),
+                                    contentDescription = "Logo",
+
+                                    )
+                            }
+                        )
+                    }
+                ) { innerPadding ->
+                    Formulario(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -92,7 +112,7 @@ fun Inicial(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun formulario(modifier: Modifier = Modifier) {
+fun Formulario(modifier: Modifier = Modifier) {
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var pass2 by remember { mutableStateOf("") }
@@ -100,16 +120,16 @@ fun formulario(modifier: Modifier = Modifier) {
 
     var context = LocalContext.current
     Column(
-        modifier = Modifier.padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
         Image(
             painter = painterResource(id = loginImageVector),
             contentDescription = "Logo",
-            //Rounded corner modifier
             contentScale = ContentScale.Crop,
             modifier = Modifier
-                .size(100.dp)
+                .size(64.dp)
                 .clip(CircleShape)
         )
         Text(
@@ -140,16 +160,29 @@ fun formulario(modifier: Modifier = Modifier) {
             //Type password
             visualTransformation = PasswordVisualTransformation(),
         )
+        //Circle progress loader
+        Spacer(modifier = Modifier.width(16.dp))
+        if (isLoading) {
+            CircularProgressIndicator(
+                modifier = Modifier.size(64.dp),
+                strokeWidth = 4.dp,
+                color = MaterialTheme.colorScheme.secondary,
+                trackColor = MaterialTheme.colorScheme.surfaceVariant
+            )
+        }
+        Spacer(modifier = Modifier.width(16.dp))
         OutlinedButton(
-            onClick = { isLoading = true },
+            onClick = {
+                isLoading = true
+                CoroutineScope(Dispatchers.Main).launch {
+                    delay(3000L)
+                    isLoading = false
+                }
+
+            },
             enabled = !isLoading,
             modifier = Modifier.padding(16.dp)
         ) {
-            if (username.isNotEmpty() && password.isNotEmpty()) {
-                CircularProgressIndicator(modifier = Modifier.size(20.dp))
-                Spacer(Modifier.width(8.dp))
-                isLoading = false
-            }
             Text(if (isLoading) "Cargando..." else "Enviar")
         }
 
@@ -160,15 +193,16 @@ fun formulario(modifier: Modifier = Modifier) {
 }
 
 
-suspend fun espera() {
-    delay(1000L)
-}
-
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        formulario()
+        Scaffold(
+            modifier = Modifier.fillMaxSize()
+        ) { innerPadding ->
+            Formulario(
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/Registro.kt
+++ app/src/main/java/com/example/myapplication/Registro.kt
@@ -1,24 +1,37 @@
-
 package com.example.myapplication
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
@@ -31,9 +44,16 @@ class Registro : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 Scaffold(
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            title = { Text("Ventana 3") },
+
+                            )
+                    }
                 ) { innerPadding ->
-                    Greeting2("Android", modifier = Modifier.padding(innerPadding))
+
+                    RegisterScreen(modifier = Modifier.padding(innerPadding))
 
                 }
             }
@@ -49,12 +69,80 @@ fun Greeting2(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun RegisterScreen(modifier: Modifier = Modifier) {
+    var correo by remember { mutableStateOf("") }
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var password2 by remember { mutableStateOf("") }
+
+    val context = LocalContext.current
+    //add topappbar
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Ventana de Registro", style = MaterialTheme.typography.headlineLarge)
+        Spacer(modifier = Modifier.padding(vertical = 16.dp))
+        Text("Username", style = MaterialTheme.typography.headlineSmall)
+        OutlinedTextField(value = username, onValueChange = { username = it })
+        Text("Correo", style = MaterialTheme.typography.headlineSmall)
+        OutlinedTextField(value = correo, onValueChange = { correo = it })
+        Text("Contraseña", style = MaterialTheme.typography.headlineSmall)
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            visualTransformation = PasswordVisualTransformation(),
+        )
+        Text("Confirme contraseña", style = MaterialTheme.typography.headlineSmall)
+        OutlinedTextField(value = password2, onValueChange = { password2 = it })
+        Spacer(modifier = Modifier.padding(vertical = 16.dp))
+
+        //Rectangle shape button with rounded corner
+        OutlinedButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.padding(16.dp)
 
 
+        ) {
+            Text("Registrar")
+
+
+        }
+
+
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview3() {
     MyApplicationTheme {
-        Greeting2("Android")
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            topBar = {
+                TopAppBar(
+                    title = { Text("Ventana 3") },
+                    navigationIcon = {
+                        IconButton(onClick = { /*Volver*/ }) {
+                            Icon(Icons.Filled.ArrowBack, null)
+
+                        }
+                    },
+                    actions={
+                        IconButton(onClick = { /* acción */ }) {
+                            Icon(Icons.Default.Menu, contentDescription = "Menu")
+                        }
+                    }
+                    )
+            }
+        ) { innerPadding ->
+
+            RegisterScreen(modifier = Modifier.fillMaxSize().padding(innerPadding))
+
+        }
     }
 }
